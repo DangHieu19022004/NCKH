878bd3336c47759af182c3ab03373c8d9bbee1fd
lequang2009k4 <lequangdmy2009k4@gmail.com>
2024-03-16 23:41:36 +0700

    fix database

---
+++ SQLQuery1.sql
@@ -0,0 +1,22 @@
+﻿--Kiểm tra ràng buộc
+EXEC sp_helpconstraint 'orders_table';
+--Xoá ràng buộc
+ALTER TABLE assignment
+DROP CONSTRAINT FK_order ;
+
+ALTER TABLE orders_table 
+DROP CONSTRAINT (constraint_name :lấy ở câu lệnh 1 ) ;
+
+-- Xóa trường cũ
+ALTER TABLE orders_table
+DROP COLUMN order_id;
+-- Thêm trường mới với kiểu tự động tăng
+ALTER TABLE orders_table
+ADD order_id INT IDENTITY(1,1) PRIMARY KEY;
+
+--thêm khoá
+
+ALTER TABLE assignment
+ADD CONSTRAINT FK_order
+FOREIGN KEY (order_id) REFERENCES orders_table(order_id);
+
--- project_Hieu/end_version_1/end_version/obj/Release/net8.0-windows/end_version.AssemblyInfo.cs
+++ project_Hieu/end_version_1/end_version/obj/Release/net8.0-windows/end_version.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("end_version")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6793c662d6ad49061126ea93e79115d2dd80a2d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188903b13250777d0b6fe6e2529c0d91a4254355")]
 [assembly: System.Reflection.AssemblyProductAttribute("end_version")]
 [assembly: System.Reflection.AssemblyTitleAttribute("end_version")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
