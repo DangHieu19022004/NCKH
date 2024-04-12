729bc5ff525261ec3595f8c8e4f5f277a534cdac
lequang2009k4 <lequangdmy2009k4@gmail.com>
2024-04-12 11:17:35 +0700

    update

---
+++ 4_assignment/project_Hieu/client/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace client
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lsvMessage = new System.Windows.Forms.ListView();
+            this.txbText = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lsvMessage
+            // 
+            this.lsvMessage.HideSelection = false;
+            this.lsvMessage.Location = new System.Drawing.Point(12, 12);
+            this.lsvMessage.Name = "lsvMessage";
+            this.lsvMessage.Size = new System.Drawing.Size(776, 367);
+            this.lsvMessage.TabIndex = 0;
+            this.lsvMessage.UseCompatibleStateImageBehavior = false;
+            this.lsvMessage.View = System.Windows.Forms.View.List;
+            // 
+            // txbText
+            // 
+            this.txbText.Location = new System.Drawing.Point(12, 385);
+            this.txbText.Multiline = true;
+            this.txbText.Name = "txbText";
+            this.txbText.Size = new System.Drawing.Size(616, 53);
+            this.txbText.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(634, 385);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(154, 53);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Client
+            // 
+            this.AcceptButton = this.btnSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txbText);
+            this.Controls.Add(this.lsvMessage);
+            this.Name = "Client";
+            this.Text = "Client";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Client_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsvMessage;
+        private System.Windows.Forms.TextBox txbText;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ 4_assignment/project_Hieu/client/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            Connect();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// gui tin di
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Send();
+            AddMessage(txbText.Text);
+        }
+
+        IPEndPoint IP;
+        Socket client;
+
+        /// <summary>
+        /// Ket noi toi sever
+        /// </summary>
+        void Connect()
+        {
+            //IP: dia chi sever
+            IP = new IPEndPoint(IPAddress.Parse("192.168.247.51"), 9999);
+            // client: lo cam de ket noi sever
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+            try
+            {
+                client.Connect(IP);
+            }
+            catch
+            {
+                MessageBox.Show("Không thể kết nối", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+
+            Thread listen = new Thread(Receive);
+            listen.IsBackground = true;
+            listen.Start();
+        }
+
+        /// <summary>
+        /// dong ket noi
+        /// </summary>
+        void Close()
+        {
+            client.Close();
+        }
+
+        /// <summary>
+        /// gui tin
+        /// </summary>
+        void Send()
+        {
+            if(txbText.Text != string.Empty)
+            {
+                client.Send(Serrialize(txbText.Text));
+            }
+        }
+
+        /// <summary>
+        /// Nhan thong tin
+        /// </summary>
+        void Receive()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 5000];
+                    client.Receive(data);
+
+                    string message = Deserialize(data).ToString();
+                    AddMessage(message);
+                }
+            }
+            catch
+            {
+                Close();
+            }
+        }
+
+        void AddMessage(string s)
+        {
+            lsvMessage.Items.Add(new ListViewItem() { Text = s});
+            txbText.Clear();
+        }
+
+        //Phan manh
+        byte[] Serrialize(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, obj);
+
+            return stream.ToArray();
+        }
+        
+        //gom manh lai
+        object Deserialize(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            return formatter.Deserialize(stream);
+        }
+
+        //dong form thi dong ket noi
+        private void Client_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
